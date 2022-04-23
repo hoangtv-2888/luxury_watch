6fe1e5b52613bc0ee7822a28ec4abab0c0a95b35
hoangtv-2888 <tran.viet.hoang@sun-asterisk.com>
2022-04-23 20:42:35 +0700

    change toast possition

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -30,3 +30,20 @@ window.I18n = I18n
 
 require("trix")
 require("@rails/actiontext")
+toastr.options = {
+    "closeButton": false,
+    "debug": false,
+    "newestOnTop": false,
+    "progressBar": false,
+    "positionClass": "toast-bottom-right",
+    "preventDuplicates": false,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+  }
